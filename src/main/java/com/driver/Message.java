bdd444da7aa325b0be0046894aaf68c29017fa5e
Sameer Malik <126045527+Sameer-Malik-git@users.noreply.github.com>
2024-06-02 15:42:38 +0530

    Update Message.java

--- src/main/java/com/driver/Message.java
+++ src/main/java/com/driver/Message.java
@@ -1,3 +1,4 @@
+
 package com.driver;
 
 import java.util.Date;
@@ -5,7 +6,44 @@ import java.util.Date;
 public class Message {
     private int id;
     private String content;
-    private Date timestamp;,,,,,
+    private Date timestamp;
 
+    public Message(){
+
+    }
+    public Message(int id,String content){
+        this.id = id;
+        this.content = content;
+        this.timestamp = new Date();
+    }
+    public Message(int id,String content , Date timestamp) {
+        this.content = content;
+        this.id = id;
+        this.timestamp = timestamp;
+
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public Date getTimestamp() {
+        return timestamp;
+    }
+
+    public void setTimestamp(Date timestamp) {
+        this.timestamp = timestamp;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
 }
-
